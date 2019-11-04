224ea59d61470c636939835672ee135fb54ba45a
michaelgayyaman <56667904+michaelgayyaman@users.noreply.github.com>
2019-11-04 12:34:55 +0800

    Add files via upload

--- CC2 - 1H - Gay-yaman, Michael Jr, L/Final Midterm Activities/Minutes.java
+++ CC2 - 1H - Gay-yaman, Michael Jr, L/Final Midterm Activities/Minutes.java
@@ -2,13 +2,51 @@ import javax.swing.JOptionPane;
 
 public class Minutes{
 	public static void main (String[] args){
-		String input;
-		int minutes1, minutes2, hours; 
+	
+		String input, yes;
+		int minutes1, minutes2, hours;
+		char y;
 		
-		input = JOptionPane.showInputDialog("Enter time in minutes: ");
-		minutes1 = Integer.parseInt(input); 
-		hours = minutes1/60; 
-		minutes2 = minutes1%60;
-		JOptionPane.showMessageDialog(null, minutes1 + " is equal to " + hours + " hours and " + minutes2 + " minute/s");
-	} 
+		do{
+			try{
+				input = JOptionPane.showInputDialog("Enter time in minutes: ");
+				
+				if(input == null){
+					
+					JOptionPane.showMessageDialog(null, "Thank You");
+					System.exit(0);
+					
+				}else if(input != null){
+					
+					minutes1 = Integer.parseInt(input);
+				
+					if(minutes1 > 0){
+
+						hours = minutes1/60;
+						minutes2 = minutes1%60;
+						
+						JOptionPane.showMessageDialog(null, minutes1 + " is equal to " + hours + " hours and " + minutes2 + " minute/s");
+					}else
+						JOptionPane.showMessageDialog(null, "Invalid Time");
+				}
+				
+			}catch(Exception e){
+				JOptionPane.showMessageDialog(null, "Invalid Input");
+			}
+			
+			yes = JOptionPane.showInputDialog("Do you want to try Again ??? (Y/N): ");
+			
+			if(yes == null){
+				JOptionPane.showMessageDialog(null, "Thank You");
+				System.exit(0);
+			}
+			
+			y = yes.charAt(0);
+			
+			
+		}while(y == 'y' || y == 'Y');
+		
+		JOptionPane.showMessageDialog(null, "Thank You");	
+	
+	}
 }
--- CC2 - 1H - Gay-yaman, Michael Jr, L/Final Midterm Activities/Shapes.java
+++ CC2 - 1H - Gay-yaman, Michael Jr, L/Final Midterm Activities/Shapes.java
@@ -1,86 +1,93 @@
 import java.util.Scanner;
-import java.lang.System;
 import java.text.DecimalFormat;
 
-public class Shapes{
-	public static void main (String[] args){
-		
+public class Shapes {
+    public static void main(String[] args) {
 
-		
 		DecimalFormat df = new DecimalFormat("0.00");
-		
-		/*System.out.println("********************");
-		
-		System.out.println("Choose a shape");
-		
-		System.out.println("1. Circle");
-		System.out.println("2. Square");
-		System.out.println("3. Rectangle");
-		System.out.println("4. Triangle\n");
-		
-		System.out.println("Choose a Number 1-4 ONLY!!!");
-		
-		System.out.println("********************");
-		
-		switch(shapes){
-			case 1:
-				System.out.println("Shape Choosen: Circle");
-				System.out.print("Enter Radius: ");
-				radius = scan.nextInt();
-				area = pi * radius * radius;
-				System.out.println("Area of the circle is: " + df.format(area));
-						
-				break;
-			case 2:
-				System.out.println("Shape Choose: Square");
-				System.out.print("Enter Sides: ");
-				side = scan.nextInt();
-				area = side * side;
-				System.out.println("Area of the square is: " + area);
-				break;
-						
-					
-		}*/
-		
-		
-		
-		String shape;
-		double pi = Math.PI, area, radius, side;
-		
-		System.out.print("Enter shape: ");
-		
-		while(true){
-			
-			Scanner scan = new Scanner(System.in);
-			
-			//System.out.println("Try Again ??? (Y / N) ");
-			
-			try{
-
-				shape = scan.nextLine();
-				
-				if(shape.equals("Circle"); || shape.equals("circle"); || shape.equals("CIRCLE");"){
-					
-					System.out.print("Enter Radius: ");
-					radius = scan.nextInt();
-					area = pi * radius * radius;
-					System.out.println("Area of the circle is: " + df.format(area));
-					
-				}else if(shape == "Square" || shape == "square" || shape == "SQUARE"){
-					System.out.print("Enter Side: ");
-				}else{
-					break;
-				}
-				
-				
-			}catch(Exception e){
-				System.out.print("Invalid Input");
-			}
-		}
-		
+	    Scanner scan = new Scanner (System.in);
 	
+	    double pi = Math.PI, radius, length, width, height, base, totalArea;
+	    String shape;
+		char yes;
+		
+		do{	
+			System.out.print("\nShapes Available in the System are:\n  - Circle\n  - Square\n  - Rectangle\n  - Triangle");
+			System.out.print("\n\nEnter Name of Shape: ");
+			shape = scan.next();
+			System.out.println();
+					
+			try{
+				switch (shape.toUpperCase()) {
+					
+					case "C":
+				    case "CIRCLE":
+						do{
+							System.out.print("Please enter VALID radius of a circle: ");
+							radius = scan. nextDouble();
+						}while(radius <= 0);
+		
+						totalArea = pi * radius * radius ;
+						System.out.println("Area of the circle is: " + df.format(totalArea) + "\n");
+						break;
+					case "S":
+					case "SQUARE":
+						do{
+							System.out.print("Please enter VALID length|width of a square: ");
+							length = scan. nextDouble ();
+						}while(length <= 0);
+								
+						totalArea = length * length;
+						System.out.println("\nArea of the square is: " + df.format(totalArea) + "\n");
+						break;						
+					
+					case "R":
+					case "RECTANGLE":
+						do{
+							System.out.print("Please enter VALID length of the rectangle: ");
+							length = scan. nextDouble ();		
+						}while(length <= 0);
+								
+						do{
+							System.out.print("\nPlease enter VALID width of a rectangle: ");
+							width = scan. nextDouble();
+						}while(width <= 0);
+								
+						totalArea = length*width;
+						System.out.println("\nArea of the rectangle is: " + df.format(totalArea) + "\n");
+						break;
+					
+					case "T":
+					case "TRIANGLE":
+						do{
+							System.out.print("Please enter VALID base|width of a triangle: ");
+							base = scan. nextDouble ();
+						}while(base <= 0);
+							
+						do{
+							System.out.print("\nPlease enter VALID height of a triangle: ");
+							height = scan. nextDouble();
+						}while(height <= 0);
+							
+						totalArea = ((base*height)/2);
+						System.out.println("\nArea of the triangle is: " + df.format(totalArea) + "\n");
+						break;
+	
+					default:
+						System.out.println("\nOnly Available Shapes in the System are:\n  - Circle\n  - Square\n  - Rectangle\n  - Triangle");
+				}
+					
+			}catch(Exception e){
+				
+				System.out.println("Invalid Input. Not a Number\n");
+				yes = scan.next().charAt(0);
+			}
+					
+			System.out.print("\tDo you want to try Again ??? (Y/N): ");
+			yes = scan.next().charAt(0);
+	
+		}while(yes == 'y' || yes == 'Y');
 		
 		
-		
-	}
+    }
 }
